8b540399e4af6726d660d6eaa501310b2b28f6fd
Masato <104467712+itsxMasato@users.noreply.github.com>
2026-02-03 17:30:57 -0600

    fix docker for backend folder

--- frontend-api/src/App.js
+++ frontend-api/src/App.js
@@ -1,29 +1,13 @@
-import { useEffect, useState } from "react";
 import Inicio from "./Paginas/Inicio";
 
-function App() {
-  const [mensaje, setMensaje] = useState("Cargando datos...");
+  
 
-  useEffect(() => {
-    // Aquí es donde sucede la magia de la conexión
-    fetch("https://miguel-flores.onrender.com/weatherforecast")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Datos recibidos:", data);
-        setMensaje("Conexión exitosa con el Backend");
-      })
-      .catch((err) => {
-        console.error("Error conectando:", err);
-        setMensaje("Error: No se pudo conectar al backend");
-      });
-  }, []);
+function App() { 
 
-  return (
-    <div>
-      <p>Estado: {mensaje}</p>
-      <Inicio />
-    </div>
-  );
-}
+  return <Inicio />; 
+
+} 
+
+  
 
 export default App;
